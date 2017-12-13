a85a0f76da2dca996bc4de3cd83516db19e415ea
peoner <peoner.nn@gmail.com>
2017-12-13 10:26:50 +0000

    123

--- INCLUDE/Kboard.h
+++ INCLUDE/Kboard.h
@@ -1,5 +1,4 @@
 
 void init_kb();
 
-extern unsigned int kb_rows[];
-extern unsigned int kb_col_row;
+extern unsigned int kb_cols[];
--- SRC/Kboard.c
+++ SRC/Kboard.c
@@ -2,38 +2,44 @@
 #include "common.h"
 #include "aduc812.h"
 
-#define row_tick 0x01010101
+#define col_tick 0x01010101
 #define magic_numb 0x40404040
 
-unsigned int kb_rows[4] = {0,0,0,0};
+unsigned int kb_cols[4] = {0,0,0,0};
 
-const unsigned int row_clean[] =
+const unsigned int col_clean[] =
     {0, 0xFF, 0xFF00, 0xFFFF,
     0xFF0000, 0xFF00FF, 0xFFFF00, 0xFFFFFF,
     0xFF000000, 0xFF0000FF, 0xFF00FF00, 0xFF00FFFF,
     0xFFFF0000, 0xFFFF00FF, 0xFFFFFF00, 0xFFFFFFFF};
 
-    unsigned int kb_tick = 0;
-    unsigned int kb_col_row = 0;
+volatile unsigned int kb_tick = 0;
+
 
 void KB_ISR(void) __interrupt(4) {
-    int row = kb_tick & 3;
-    unsigned int buff;
-    write_max(KB, ~(1 << 0));
+    unsigned int buff, kb_col_row;
+    int col = kb_tick & 3;
+
+    write_max(KB, ~(1 << col));
     kb_col_row = read_max(KB);
 
-     buff = kb_rows[row];
-     buff += row_tick;
-     buff &=  row_clean[( ~(kb_col_row >> 4) & 15)];
+     buff = kb_cols[col];
+     buff += col_tick;
+     buff &=  col_clean[( ~(kb_col_row >> 4) & 15)];
 
-    if(kb_tick>64){
+    if(kb_tick>64*4){
         buff &= 0x7f7f7f7f;     //00->00;01->10;10->00;11->10
         buff += magic_numb;
         buff &= 0xbfbfbfbf;
     }
-    WriteLED(kb_rows[0]);
-    kb_rows[row] = buff;
+	if(kb_tick > 64*4+4)
+		kb_tick = 0;
+
+    kb_cols[col] = buff;
+	//WriteLED(kb_cols[0]);
     kb_tick ++;
+	TH0 = 247;
+	TL0 = 0x00;
 }
 
 void init_kb(){
